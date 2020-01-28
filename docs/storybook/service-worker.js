59236998ab7332bdbb5b3d9d88d434e94b98c31d
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2020-01-28 16:49:50 -0700

    Fixed missing scripts.

--- docs/storybook/service-worker.js
+++ docs/storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9b459cc53b911dc428a37e0ff2f7b772.js"
+  "precache-manifest.7dae31d7d37523612444b31adff687ea.js"
 );
 
 self.addEventListener('message', (event) => {
