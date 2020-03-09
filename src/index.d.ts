b9769206900e63f3d9bb5e7a4867780312a1da17
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2020-03-09 17:34:41 -0600

    Added a typescript file.

---
+++ src/index.d.ts
@@ -0,0 +1,40 @@
+import React, { CSSProperties } from "react";
+
+type KeyValue = {
+  [key: string]: string | number;
+};
+
+type States = {
+  [key: string]: CSSProperties;
+};
+
+type TransitionOptions<TElement, TAnimatedProperties> = {
+  duration: number;
+  applyValues?: (obj: any, values: KeyValue) => void;
+  ref?: React.Ref<TElement>;
+  animate?: boolean;
+  onComplete?: () => void;
+  initialProperties?: TAnimatedProperties;
+};
+
+type StyledTranstionOptions<TElement, TAnimatedProperties> = {
+  duration?: number;
+  props?: any;
+  ref?: React.Ref<TElement>;
+  animate?: boolean;
+  onComplete?: () => void;
+  initialProperties?: TAnimatedProperties;
+};
+
+export declare function useTransition<T = any, TAnimatedProperties = KeyValue>(
+  animatedProperties: TAnimatedProperties,
+  options: TransitionOptions<T, TAnimatedProperties>
+): React.Ref<T> | React.RefObject<T>;
+
+export declare function makeStyledTransition<TElement>(
+  states: States,
+  duration: number
+): (
+  stateName: string,
+  options?: StyledTranstionOptions<TElement, CSSProperties>
+) => React.Ref<TElement>;
