aa7c6ee4d0224632a6e7cd16d096dbc48612b6ec
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2020-09-23 23:57:08 -0600

    Refactored the hook to use a mediator instead. This cleaned up the code significantly within the hook.

--- src/hooks/useNativeTransition/index.js
+++ src/hooks/useNativeTransition/index.js
@@ -1,5 +1,5 @@
 import { useState, useCallback, useEffect } from "react";
-import transfromAnimatedProperties from "../useTransition/transformAnimatedProperties";
+import transfromAnimatedProperties from "./transformAnimatedProperties";
 import { bezierCurveEasings } from "../useTransition/easeOut";
 
 const useNativeTransition = (
--- src/hooks/useTransition/index.js
+++ src/hooks/useTransition/index.js
@@ -1,18 +1,6 @@
-import { useEffect, useRef, useCallback } from "react";
-import { Timeline } from "motion-ux";
-import createAnimations from "./createAnimations";
-import createAdjustedAnimations from "./createAdjustedAnimations";
-import isEqual from "../isEqual";
-import transformAnimatedProperties from "./transformAnimatedProperties";
+import { useEffect, useMemo, useCallback } from "react";
 import objectApplyValues from "./objectApplyValues";
-import assertAnimatingTheSameProperties from "./assertAnimatingTheSameProperties";
-
-const convertToValues = animatedProperties => {
-  return Object.keys(animatedProperties).reduce((properties, key) => {
-    properties[key] = animatedProperties[key].value;
-    return properties;
-  }, {});
-};
+import TransitionMediator from "./TransitionMediator";
 
 const useTransition = (
   animatedProperties,
@@ -23,148 +11,47 @@ const useTransition = (
     animate = true,
     onComplete,
     configure,
-    initialProperties = null
+    initialProperties = null,
   } = {}
 ) => {
-  const state = useRef({
-    lastAnimatedProperties: null,
-    animatedProperties,
-    duration,
-    applyValues,
-    ref,
-    animate,
-    timeline: null,
-    node: null
-  });
+  const transitionMediator = useMemo(() => {
+    return new TransitionMediator();
+  }, []);
 
-  if (initialProperties != null && state.current.lastAnimatedProperties == null) {
-    transformAnimatedProperties(initialProperties);
-    state.current.lastAnimatedProperties = initialProperties;
-  }
-
-  state.current.animatedProperties = animatedProperties;
-  state.current.duration = duration;
-  state.current.applyValues = applyValues;
-  state.current.ref = ref;
-  state.current.animate = animate;
+  transitionMediator.setInitialProperties(initialProperties);
+  transitionMediator.setDuration(duration);
+  transitionMediator.setRenderCallback(applyValues);
+  transitionMediator.setShouldAnimate(animate);
+  transitionMediator.setOnCompleteCallback(onComplete);
+  transitionMediator.setConfigureCallback(configure);
 
   // Keep refs up to date.
-  const callbackRef = useCallback(node => {
-    const { animatedProperties, applyValues, ref } = state.current;
+  const callbackRef = useCallback(
+    (node) => {
+      if (node != null) {
+        if (typeof ref === "function") {
+          ref(node);
+        } else if (
+          typeof ref === "object" &&
+          ref != null &&
+          ref.hasOwnProperty("current")
+        ) {
+          ref.current = node;
+        }
 
-    if (node != null) {
-      if (typeof ref === "function") {
-        ref(node);
-      } else if (
-        typeof ref === "object" &&
-        ref != null &&
-        ref.hasOwnProperty("current")
-      ) {
-        ref.current = node;
+        transitionMediator.setNode(node);
       }
-
-      state.current.node = node;
-
-      // This should run when the element is mounted.
-      const values = convertToValues(animatedProperties);
-
-      applyValues(state.current.node, values);
-      state.current.lastAnimatedProperties = animatedProperties;
-    }
-  }, []);
-
-  // Clean up timeline to prevent memory leak.
-  useEffect(() => {
-    return () => {
-      if (state.current.timeline != null) {
-        state.current.timeline.dispose();
-      }
-    };
-  }, []);
-
-  if (animatedProperties == null) {
-    // Stop the current animation, if there is one.
-    if (state.current.timeline != null) {
-      state.current.timeline.dispose();
-    }
-
-    // Reset
-    state.current.lastAnimatedProperties = null;
-    return callbackRef;
-  }
-
-  transformAnimatedProperties(animatedProperties);
-
-  const isDifferent = !isEqual(
-    animatedProperties,
-    state.current.lastAnimatedProperties
+    },
+    [transitionMediator, ref]
   );
 
-  if (state.current.lastAnimatedProperties == null || !animate) {
-    if (state.current.timeline != null) {
-      state.current.timeline.dispose();
-      state.current.timeline = null;
-    }
+  useEffect(() => {
+    return () => {
+      transitionMediator.dispose();
+    };
+  }, [transitionMediator]);
 
-    const values = convertToValues(animatedProperties);
-
-    if (state.current.node != null) {
-      applyValues(state.current.node, values);
-    }
-
-    state.current.lastAnimatedProperties = animatedProperties;
-    return callbackRef;
-  } else if (isDifferent && state.current.lastAnimatedProperties != null) {
-    assertAnimatingTheSameProperties(
-      animatedProperties,
-      state.current.lastAnimatedProperties
-    );
-
-    if (state.current.timeline == null) {
-      const animations = createAnimations(
-        state.current.lastAnimatedProperties,
-        animatedProperties
-      );
-
-      state.current.timeline = new Timeline({
-        animations: animations,
-        duration: duration
-      });
-    } else {
-      const animations = createAdjustedAnimations(
-        state.current.timeline,
-        state.current.lastAnimatedProperties,
-        animatedProperties
-      );
-
-      state.current.timeline.dispose();
-
-      state.current.timeline = new Timeline({
-        animations: animations,
-        duration: duration
-      });
-    }
-
-    if (typeof configure === "function") {
-      configure(state.current.timeline);
-    }
-
-    state.current.timeline.observe("RENDER", ({ animations }) => {
-      if (state.current.node != null) {
-        applyValues(state.current.node, animations.useTransition);
-      }
-    });
-
-    state.current.timeline.observeTime(1, () => {
-      state.current.timeline.current = null;
-      if (typeof onComplete === "function") {
-        onComplete();
-      }
-    });
-
-    state.current.timeline.play();
-    state.current.lastAnimatedProperties = animatedProperties;
-  }
+  transitionMediator.updateAnimationProperties(animatedProperties);
 
   return callbackRef;
 };
